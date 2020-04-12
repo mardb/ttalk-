fea46e6f6771b6f250181bd4b5f679e10f2d3778
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-12 15:55:25 -0700

    setting up routes

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -6,9 +6,6 @@ const Layout = ({children}) => {
     <ul className="nav nav-tabs bg-secondary ">
       <li className="nav-item">
         <a href="/" className="text-light nav-link">Home</a>
-        <a href="/" className="text-light nav-link">About</a>
-        <a href="/" className="text-light nav-link">Signin</a>
-        <a href="/" className="text-light nav-link">Logout</a>
       </li>
     </ul>
   )
