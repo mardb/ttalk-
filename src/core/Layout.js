ae12c76b7757754a1a23bcbeebe221eeac718a20
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-12 16:20:34 -0700

    navigation bar skeleton, no logic

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -1,13 +1,28 @@
 //fragment allows multiple react elements  in jsx
 import React, {Fragment} from 'react';
+import {Link} from 'react-router-dom';
 
 const Layout = ({children}) => {
   const nav = () => (
     <ul className="nav nav-tabs bg-secondary ">
       <li className="nav-item">
-        <a href="/" className="text-light nav-link">Home</a>
+        <Link href="/" className="text-white nav-link">Home</Link>
       </li>
+
+      <Fragment>
+        <li className="nav-item">
+            <Link to="/signin" className="nav-link text-light">
+                Signin
+            </Link>
+        </li>
+        <li className="nav-item">
+            <Link to="/signup" className="nav-link text-white">
+                Signup
+            </Link>
+        </li>
+      </Fragment>
     </ul>
+    
   )
 
   return(
