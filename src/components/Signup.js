9e6830d8ddfc02105c044052e3c3ce7e48ee9322
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-13 14:59:08 -0700

    FINALLY connected signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,64 +1,101 @@
-import React, {useState} from 'react';
-// import {Link, Redirect} from 'react-router-dom';
-// import axios from 'axios';
-import Layout from '../core/Layout';
-import{ToastContainer
-  // , toast
-} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
+import React, { useState } from "react";
+import axios from "axios";
+import Layout from "../core/Layout";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 const Signup = () => {
-    const [values] = useState({
-        name: "mar",
-        email: "m@go.com",
-        password: "111111",
-        buttonText: "Submit"
-    });
+  const [values, setValues] = useState({
+    name: "marlene",
+    email: "marleneguzmanla@gmail.com",
+    password: "111111",
+    buttonText: "Submit",
+  });
 
-    const {name, email, password, buttonText} = values
+  const { name, email, password, buttonText } = values;
 
-    const handleChange= (name) => (event) => {
+  const handleChange = (name) => (event) => {
+    console.log(event.target.value);
+    setValues({ ...values, [name]: event.target.value });
+  };
 
-    }
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API}/signup`,
+      data: { name, email, password },
+    })
+      .then((response) => {
+        console.log("You have successfully signed up!", response);
+        //clean up state and empty values
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          buttonText: "Submitted",
+        });
+        toast.success(response.data.message);
+      })
+      .catch((error) => {
+        console.log("There was an error signing up.", error.response.data);
+        setValues({ ...values, buttonText: "Submit" });
+        toast.error(error.response.data.error);
+      });
+  };
 
-    const clickSubmit = event => {
+  const signupForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleChange("name")}
+          value={name}
+        />
+      </div>
 
-    }
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          onChange={handleChange("email")}
+          value={email}
+        />
+      </div>
 
-const signupForm = () => (
-        <form>
-            <div className="form-group">
-                <lable className="text-muted">Name</lable>
-                <input type="text" className="form-control" onChange={handleChange("name")} value={name} />
-            </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          onChange={handleChange("password")}
+          value={password}
+        />
+      </div>
 
-            <div className="form-group">
-                <lable className="text-muted">Email</lable>
-                <input type="email" className="form-control" onChange={handleChange(email)} value={email}/>
-            </div>
+      <div>
+        <button className="btn btn-secondary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
+      </div>
+    </form>
+  );
 
-            <div className="form-group">
-                <lable className="text-muted">Password</lable>
-                <input type="password" className="form-control" onChange={handleChange(password)} value={password}/>
-            </div>
-
-            <div>
-                <button className="btn btn-secondary" onClick={clickSubmit}>
-                   {buttonText} 
-                </button>
-            </div>
-        </form>
-    );
-
-    return (
-        <Layout>
-            <div className="col-md-6 offset-md-3">
-                <ToastContainer />
-                <h1 className="p-5 text-center">Signup</h1>
-                {signupForm()}
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <ToastContainer />
+        {JSON.stringify({ name, email, password })}
+        <h1 className="p-5 text-center">Signup</h1>
+        {signupForm()}
+      </div>
+    </Layout>
+  );
 };
 
 export default Signup;
