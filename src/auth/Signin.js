91fb5a602ff9b2d972c88c924c0969649aa1d20a
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-12 17:44:44 -0700

    making sign in routes

---
+++ src/auth/Signin.js
@@ -0,0 +1,25 @@
+import React
+// , {useState} 
+from 'react';
+// import {Link, Redirect} from 'react-router-dom';
+// import axios from 'axios';
+import Layout from '../core/Layout';
+import{ToastContainer
+  // , toast
+} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+const Signin = () => {
+
+return(
+  <Layout>
+  <div className="col-md-6 offset-md-3">
+      <ToastContainer />
+      <h1 className="p-5 text-center">Signin</h1>
+  </div>
+</Layout>
+)
+};
+
+export default Signin;
+
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import App from '../App'
-import Signup from '../auth/Signup'
-import About from '../auth/About'
+import App from '../App';
+import Signup from '../auth/Signup';
+import About from '../auth/About';
+import Signin from '../auth/Signin';
 
 const Routes = () => {
   return(
@@ -11,7 +12,7 @@ const Routes = () => {
           <Route path="/" exact component={App}/>
           <Route path="/about" exact component= {About}/>
           <Route path="/signup" exact component= {Signup}/>
-          {/* <Route path="/signin" exaact component= {Signin}/> */}
+          <Route path="/signin" exaact component= {Signin}/>
           {/* <Route path="/signout" exact component= {Signout}/> */}
         </Switch>
       </BrowserRouter>
