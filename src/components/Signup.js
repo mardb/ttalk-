a57d88fea74174d052a975ec5fce9e46f0553f98
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-12 21:06:23 -0700

    signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,25 +1,54 @@
 import React
-// , {useState} 
 from 'react';
-// import {Link, Redirect} from 'react-router-dom';
-// import axios from 'axios';
 import Layout from '../core/Layout';
-import{ToastContainer
-  // , toast
-} from 'react-toastify';
+import{ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const Signup = () => {
 
-return(
-  <Layout>
-  <div className="col-md-6 offset-md-3">
-      <ToastContainer />
-      <h1 className="p-5 text-center">Signup</h1>
-  </div>
-</Layout>
-)
+// return(
+//   <Layout>
+//   <div className="col-md-6 offset-md-3">
+//       <ToastContainer />
+//       <h1 className="p-5 text-center">Signup</h1>
+//   </div>
+// </Layout>
+// )
+const signupForm = () => (
+        <form>
+            <div className="form-group">
+                <lable className="text-muted">Name</lable>
+                <input type="text" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <lable className="text-muted">Email</lable>
+                <input type="email" className="form-control" />
+            </div>
+
+            <div className="form-group">
+                <lable className="text-muted">Password</lable>
+                <input type="password" className="form-control" />
+            </div>
+
+            <div>
+                <button className="btn btn-secondary" >
+                   Signup
+                </button>
+            </div>
+        </form>
+    );
+
+    return (
+        <Layout>
+            <div className="col-md-6 offset-md-3">
+                <ToastContainer />
+                <h1 className="p-5 text-center">Signup</h1>
+                {signupForm()}
+            </div>
+        </Layout>
+    );
 };
 
 export default Signup;
-
+// **
