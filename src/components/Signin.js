366e2ddef9b703a459ac8d253497291ff589954e
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-13 17:19:01 -0700

    adding functionality to the signin page and connecting it with the back end

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,31 +1,74 @@
-import React from // , {useState}
-"react";
-// import {Link, Redirect} from 'react-router-dom';
-// import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 import Layout from "../core/Layout";
-import {
-  ToastContainer,
-  // , toast
-} from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 
+
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    buttonText: "Submit",
+  });
+
+  const { email, password, buttonText } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, buttonText: "Submitting" });
+    axios({
+      method: "POST",
+      url: `${process.env.REACT_APP_API}/signin`,
+      data: { email, password },
+    })
+      .then((response) => {
+        console.log("You have successfully signed in!", response);
 
 
-  
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          buttonText: "Submitted",
+        });
+        toast.success(`Hi ${response.data.user.name}, Welcome back!`);
+      })
+      .catch((error) => {
+        console.log("There was an error signing in!", error.response.data);
+        setValues({ ...values, buttonText: "Submit" });
+        toast.error(error.response.data.error);
+      });
+  };
+
   const signinForm = () => (
     <form>
       <div className="form-group">
-        <lable className="text-muted">Email</lable>
-        <input type="email" className="form-control"/>
+        <label className="text-muted">Email</label>
+        <input
+          type="email"
+          className="form-control"
+          onChange={handleChange("email")}
+        />
       </div>
 
       <div className="form-group">
-        <lable className="text-muted">Password</lable>
-        <input type="password" className="form-control"/>
+        <label className="text-muted">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          onChange={handleChange("password")}
+        />
       </div>
       <div>
-        <button className="btn btn-secondary">Signin</button>
+        <button className="btn btn-secondary" onClick={clickSubmit}>
+          {buttonText}
+        </button>
       </div>
     </form>
   );
@@ -42,4 +85,4 @@ const Signin = () => {
 };
 
 export default Signin;
-
+// ***
