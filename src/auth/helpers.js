f40e35643f22d794a4624afc5b5e1ddd2e98bf84
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-14 14:12:44 -0700

    cookies!!

---
+++ src/auth/helpers.js
@@ -0,0 +1,17 @@
+import cookie from 'js-cookie'
+
+//save cookie in browser cookie 
+
+//signs user out and removes browser cook 
+
+//stored stoke
+
+//token request to server
+
+//local storage
+
+//delete local storage
+
+// auth user during sign in 
+
+//  retrieve user info 
