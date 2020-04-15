8b9071b108a9eaef87a3dcd96a8e469a8275c4a7
Marlene Guzman <guzmanmarlene@yahoo.com>
2020-04-15 08:38:47 -0700

    user private page

--- src/core/Private.js
+++ src/core/Private.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import Layout from './Layout'
+import React from "react";
+import Layout from "./Layout";
 
 const Private = () => (
-<Layout>
-  <h1>I am the signed in user. this is my home page. </h1>
-</Layout>
-)
+  <Layout>
+    <h1>I am the signed in user. this is my home page. </h1>
+    <div>
+      <div className="jumbotron">
+        <h2>Home</h2>
+        <p className="lead">Welcome to my Blog</p>
+      </div>
+      <div className="container">
+        <h1>Posts will go here</h1>
+      </div>
+    </div>
+  </Layout>
+);
 
 export default Private;
